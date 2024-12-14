19f98b626014564e4d05acebc93a055c3c20af0b
andkuzm <kandrei704@gmail.com>
2024-12-14 14:03:08 +0200

    ContactUs.vue added

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div id="ContactUsContainer" style="padding-top: 10%; border-radius: 2rem; background-color: lightgray;">
+    <p style="text-align: center;">Contact us:</p>
+    <p style="text-align: center;">by phone 6555555</p>
+    <p style="text-align: center;">by mail mail@mail.com</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
